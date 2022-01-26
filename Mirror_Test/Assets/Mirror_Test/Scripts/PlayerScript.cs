7479d8f5b8ca912b05f06c74110e5aa684f1d599
masaywar <masaywar@gmail.com>
2022-01-26 16:54:36 +0900

    init commit

---
+++ Mirror_Test/Assets/Mirror_Test/Scripts/PlayerScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerScript : NetworkBehaviour
+{
+    public override void OnStartLocalPlayer()
+    {
+        Camera.main.transform.SetParent(transform);
+        Camera.main.transform.localPosition = Vector3.zero;
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer) 
+        {
+            return;
+        } 
+
+        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 110.0f;
+        float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * 4f;
+        
+        transform.Rotate(0, moveX, 0);
+        transform.Translate(0, 0, moveZ);
+    }
+}
